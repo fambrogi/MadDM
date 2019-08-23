37169803eb2d10640b947e20acdc4439f0de8565
Federico Ambrogi <federico.ambrogi88@gmail.com>
2019-08-23 15:58:40 +0200

    up

--- EW_Study/August_2019_Latest/EW_Compare_a_z_h.py
+++ EW_Study/August_2019_Latest/EW_Compare_a_z_h.py
@@ -80,7 +80,7 @@ def Plot(DM, dic_EW = '', dic_noEW = '', CR = '', channel = '' , scaled = False)
 
 
     plt.text(0.000002, 180 , r'$ m_{\tilde \chi_1 ^0}=$ ' + DM_f + ' TeV' , size=SIZE-5, ha="left", va="center" , color = 'red')
-    plt.text(0.000002, 390 , r'$\tilde \chi_1 ^0  \tilde \chi_1 ^0 \rightarrow W^+ W^- Z $' , size=SIZE-5, ha="left", va="center" , color = 'red')
+    plt.text(0.000002, 390 , r'$\tilde \chi_1 ^0  \tilde \chi_1 ^0 \rightarrow W^+ W^-  $' , size=SIZE-5, ha="left", va="center" , color = 'red')
     plt.text(0.000002, 100 , label_CR , size=SIZE-5, ha="left", va="center", color = 'red')
 
     width = 1.2
@@ -95,7 +95,7 @@ def Plot(DM, dic_EW = '', dic_noEW = '', CR = '', channel = '' , scaled = False)
     
     
 
-    tree = r'MadDM $\tilde \chi_1 ^0 \tilde \chi_1 ^0  \rightarrow W^-W^+ $'
+    tree = r'$\tilde \chi_1 ^0 \tilde \chi_1 ^0  \rightarrow W^-W^+ $'
            
 
 
@@ -110,12 +110,28 @@ def Plot(DM, dic_EW = '', dic_noEW = '', CR = '', channel = '' , scaled = False)
         handles.append( a[0] )
         handles.append( b[0] )
 
-        first_legend = plt.legend(handles= handles, fontsize=SIZE-10, loc = 'upper right', fancybox=True , ncol = 1)
+        first_legend = plt.legend(handles= handles, fontsize=SIZE-12, loc = 'lower right', fancybox=True , ncol = 1)
         frame = first_legend.get_frame()
         frame.set_edgecolor('gray')
         ax = plt.gca().add_artist(first_legend)
 
-        handles = []
+
+ 	de, y   = READ('Results/Data_n1n1_ww_'+DM_f+'TeV/'  + Name_Dic[CR] + '_test1000GeV_ww_lhe.dat')
+ 	de, y_z = READ('Results/Data_n1n1_wwz_'+DM_f+'TeV/' + Name_Dic[CR] + '_test1000GeV_ww_lhe.dat')
+  	de, y_h = READ('Results/Data_n1n1_wwh_'+DM_f+'TeV/' + Name_Dic[CR] + '_test1000GeV_ww_lhe.dat')
+   	de, y_a = READ('Results/Data_n1n1_wwa_'+DM_f+'TeV/' + Name_Dic[CR] + '_test1000GeV_ww_lhe.dat')
+        x = np.array([ math.pow(10,num) for num in de]) 
+        handles = [] 
+        somma = []
+    	for y, ya, yz, yh  in zip (y, y_a, y_z, y_h):
+            s = y * xsections_dic['a'][DM_f]['rel'][0]  + yz * xsections_dic['z'][DM_f]['rel'][1] + ya * xsections_dic['a'][DM_f]['rel'][1] + yh * xsections_dic['h'][DM_f]['rel'][1]
+            somma.append(s) 
+        xsec = xsections_dic['a'][DM_f]['abs'][0] + xsections_dic['z'][DM_f]['abs'][1] +  xsections_dic['a'][DM_f]['abs'][1] +  xsections_dic['h'][DM_f]['abs'][1] 
+ 	g = plt.plot(x, somma, label = r'Sum $(tree+ \gamma +z+h)$[' + str(xsec) + 'pb]', color = 'magenta', ls = '-')	    
+   	handles.append(g[0])
+
+
+
         # Samples with photons
    
         dic = {}
@@ -126,14 +142,24 @@ def Plot(DM, dic_EW = '', dic_noEW = '', CR = '', channel = '' , scaled = False)
 		  'Data_n1n1_wwa_' + DM_f + 'TeV' ,
 	          'Data_n1n1_wwh_' + DM_f + 'TeV' , 
 	          'Data_n1n1_wwz_' + DM_f + 'TeV' , 
-		  'Data_n1n1_wwx_' + DM_f + 'TeV' , ]
+		  'Data_n1n1_wwx_' + DM_f + 'TeV' , 
+		  'Data_n1n1_wwwwx_' + DM_f + 'TeV' , 
+]
 
-        COLORS = ['blue', 'yellow', 'limegreen', 'cyan', 'orange']
+        COLORS = ['blue', 'yellow', 'red', 'cyan', 'orange', 'lime']
 
-        xsections_abs = [ xsections_dic['z'][DM_f]['abs'][0], xsections_dic['a'][DM_f]['abs'][1], xsections_dic['h'][DM_f]['abs'][1], xsections_dic['z'][DM_f]['abs'][1] ,xsections_dic['x'][DM_f]['abs'][1]  ]
-        xsections_rel = [ xsections_dic['z'][DM_f]['rel'][0], xsections_dic['a'][DM_f]['rel'][1], xsections_dic['h'][DM_f]['rel'][1], xsections_dic['z'][DM_f]['rel'][1] ,xsections_dic['x'][DM_f]['rel'][1]  ]
+        COLORS = ['slateblue', 'cyan', 'lime', 'yellow', 'orange', 'blue']
 
-        LABELS = [ tree , tree + r'$\gamma$' , tree + r'$h$' , tree + r'$z$' , tree + r'$x$' ]
+        xsections_abs = [ xsections_dic['z'][DM_f]['abs'][0], xsections_dic['a'][DM_f]['abs'][1], xsections_dic['h'][DM_f]['abs'][1], xsections_dic['z'][DM_f]['abs'][1], xsections_dic['x'][DM_f]['abs'][1],
+			  float(xsections_dic['all'][DM_f]['abs'][1])     ]
+
+        xsections_rel = [ xsections_dic['z'][DM_f]['rel'][0], xsections_dic['a'][DM_f]['rel'][1], xsections_dic['h'][DM_f]['rel'][1], xsections_dic['z'][DM_f]['rel'][1] ,xsections_dic['x'][DM_f]['rel'][1],
+			  float(xsections_dic['all'][DM_f]['abs'][1])/ xsections_dic['all'][DM_f]['abs'][0]   ]
+
+
+
+
+        LABELS = [ tree , tree + r'$\gamma$' , tree + r'$h$' , tree + r'$z$' , tree + r'$x$' , tree + '+' r'$W^-W^- X$' ]
 
         for F,C,L,s,so in zip (FILES, COLORS, LABELS, xsections_rel, xsections_abs):
 
@@ -143,7 +169,7 @@ def Plot(DM, dic_EW = '', dic_noEW = '', CR = '', channel = '' , scaled = False)
             x = np.array([ math.pow(10,num) for num in x]) 
             
    	    if scaled:
-		scale_tag = r'($\sigma=$' + str(so) + '[pb])'
+		scale_tag = r' [' + str(so) + 'pb]'
                 y = np.array(y) * s
                 dic[COLORS.index(C)] = y
    	    else:
@@ -153,22 +179,14 @@ def Plot(DM, dic_EW = '', dic_noEW = '', CR = '', channel = '' , scaled = False)
             handles.append( a[0] )
 
         
-        somma  = []
 
-    de, y   = READ('Results/Data_n1n1_ww_'+DM_f+'TeV/'  + Name_Dic[CR] + '_test1000GeV_ww_lhe.dat')
-    de, y_z = READ('Results/Data_n1n1_wwz_'+DM_f+'TeV/' + Name_Dic[CR] + '_test1000GeV_ww_lhe.dat')
-    de, y_h = READ('Results/Data_n1n1_wwh_'+DM_f+'TeV/' + Name_Dic[CR] + '_test1000GeV_ww_lhe.dat')
-    de, y_a = READ('Results/Data_n1n1_wwa_'+DM_f+'TeV/' + Name_Dic[CR] + '_test1000GeV_ww_lhe.dat')
-        
-    for y, ya, yz, yh  in zip (y, y_a, y_z, y_h):
-            s = y * xsections_dic['a'][DM_f]['rel'][0] + yz * xsections_dic['z'][DM_f]['rel'][1] + ya * xsections_dic['a'][DM_f]['rel'][1] + yh * xsections_dic['h'][DM_f]['rel'][1]
-            somma.append(s) 
 
-    g = plt.plot(x, somma, label = r'Sum $(tree+ \gamma +z+h)$', color = 'magenta', ls = '-')	    
-    handles.append(g[0])
+
         
 
-    second_legend = plt.legend(handles= handles, fontsize=SIZE-10, loc = 'lower left', fancybox=True , ncol = 1)
+        
+
+    second_legend = plt.legend(handles= handles, fontsize=SIZE-12, loc = 'upper right', fancybox=True , ncol = 1)
     frame = second_legend.get_frame()
     frame.set_edgecolor('gray')
     ax = plt.gca().add_artist(second_legend)
@@ -181,7 +199,7 @@ def Plot(DM, dic_EW = '', dic_noEW = '', CR = '', channel = '' , scaled = False)
              plt.plot(Fermi_limits[0],Fermi_limits[1] , color = 'gray', linestyle = '--')
              place = 0.3
              if DM == '1': place = 10
-             plt.text( 0.5/float(DM) + 0.5/float(DM)/5 , place, 'Fermi Sensitivity'  , rotation=90, fontsize = 13 , color ='gray' )
+             plt.text( 0.5/float(DM) + 0.5/float(DM)/5 , place, 'Fermi Sensitivity'  , rotation=90, fontsize = 11 , color ='gray' )
              plt.text( 0.5/float(DM) + 0.5/float(DM)/5 , 0.013, r'$\longrightarrow$' , fontsize = 20 , color ='gray' )
 
     plt.grid(which='major', linestyle=':', color = 'lightgray', lw = 1.05)
@@ -206,7 +224,7 @@ CRs = ['gammas','positrons','neutrinos_e', 'neutrinos_tau' , 'neutrinos_mu' , 'a
 #CRs = ['gammas',]
 
 
-DMs = ['1000.0' ]
+
 for m in DMs:
 
     for c in CRs:
